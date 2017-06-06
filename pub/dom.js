15abfb3e133c751673f966c5f4c7f54f15f1c953
Rav-T <hiddenkrypt@gmail.com>
2017-06-06 13:29:50 -0700

    builing 'attract mode' to play before logging in, and to build more of the groundwork for actual gameplay

--- pub/dom.js
+++ pub/dom.js
@@ -1,27 +1,61 @@
 //dom.js
 "use strict"
 
-let dom = {
-	init: ()=>{
-		dom.gameboard = document.getElementById("gameboard")
-		dom.grid = new Array(5).fill(0)
-		dom.grid.map((e,x)=>{
+let dom = new (function(){
+	let gameboard = {};
+	let grid = new Array(5).fill(0)
+	this.init = ()=>{
+		buildGameGrid()
+	};
+	this.reset = ()=>{
+		grid.forEach(r=>{
+			r.forEach(card=>{
+				card.setWord('*');
+				card.setColor("hidden");
+			})
+		})
+	}
+	this.setWord = (x,y,word)=>{
+		grid[x][y].setWord(word)
+	}
+	this.reveal = (x,y,team)=>{
+		grid[x][y].setColor(team);
+	}
+	function Cell(x,y){
+		let teamColors = {
+			"Gold":	"#a0ae00",
+			"Purple": "#500070",
+			"Civilian": "#929292",
+			"Assassin": "#323232",
+			"hidden": "#eee"
+		}
+		this.el = document.getElementById(`c${x}${y}`)
+		this.setColor = (team)=>{
+			//console.log(`setting ${x}:${y} to ${team}:${teamColors[team]}`)
+			this.el.style.backgroundColor = teamColors[team]
+		}
+		this.setWord = (word)=>{
+			this.el.innerHTML = word
+		}
+			
+	}
+	function buildGameGrid(){
+		gameboard = document.getElementById("gameboard")
+		grid = grid.map((e,x)=>{
 			e=[]
 			let row = document.createElement("div")
 			row.id=`r${x}`
 			row.classList.add('row')
-			dom.gameboard.appendChild(row)
+			gameboard.appendChild(row)
 			for(let y=0; y<5; y++){
 				let cell = document.createElement("div")
 				cell.id = `c${x}${y}`
 				cell.classList.add('card')
 				cell.appendChild(document.createTextNode("*"));
 				row.appendChild(cell)
-				console.log(typeof e);
-				console.log(e);
-				e.push(document.getElementById(`c${x}${y}`));
+				e.push(new Cell(x,y))
 			}
 			return e
 		})
 	}
-}
+})()
--- pub/engine.js
+++ pub/engine.js
@@ -2,9 +2,58 @@
 "use strict"
 
 let engine = (()=>{
+	let attractMode = true;
+	
 	window.onload = ()=>{
 		dom.init()
-	};
-	console.log("new engine running")
-
+		startAttractMode()
+	}
+	
+	function startAttractMode(){
+	
+		let attractModeKey = new Array(5).fill("x").map(e=>new Array(5).fill('x'));
+		
+		function setupAttractGame(){
+			dom.reset();
+			let names = ["movie", "equipment", "physics", "analysis", "policy", "series", "thought", "basis", "boyfriend", "direction", "strategy", "technology", "army", "camera", "freedom", "paper", "environment", "child", "instance", "month", "truth", "marketing", "university", "writing", "article", "department", "difference", "goal", "news", "audience", "fishing", "growth", "income", "marriage", "user", "combination", "failure", "meaning", "medicine", "philosophy", "teacher", "night", "chemistry", "disease", "disk", "energy", "nation", "road", "role", "soup", "location", "success", "addition", "apartment", "education", "math", "moment", "painting", "politics", "attention", "decision", "event", "property", "shopping", "student", "wood", "office", "population", "president", "unit", "category", "cigarette", "context", "driver", "flight", "length", "magazine", "newspaper", "teaching", "cell", "dealer", "finding", "lake"];		
+			names.sort(e=>Math.random()>.5);
+			for(let x=5;x--;){
+				for(let y=5; y--;){
+					dom.setWord(x,y,names.pop())
+				}
+			}
+			let pattern = ["Gold","Gold","Gold","Gold","Gold","Gold","Gold","Gold","Gold","Purple","Purple","Purple","Purple","Purple","Purple","Purple","Purple","Civilian","Civilian","Civilian","Civilian","Civilian","Civilian","Civilian","Assassin"];
+			let game = pattern.slice().sort(e=>Math.random()<.5);
+			for(let x=5;x--;){
+				for(let y=5; y--;){
+					let p = game.pop()
+					attractModeKey[x][y] = p
+					console.log(`set ${x}:${y} to ${p}`)
+					console.log(`remaining: ${game.length}`)
+				}
+			}
+			if(attractMode){
+				setTimeout(setupAttractGame,24500)
+			}
+			
+		}
+		function tick(){
+			let pick = null;
+			let randX = 0
+			let randY = 0
+			while (pick==null){ 
+				randX=Math.floor(Math.random()*5)
+				randY=Math.floor(Math.random()*5)
+				pick = attractModeKey[randX][randY]
+			}
+			dom.reveal(randX,randY,pick);
+			attractModeKey[randX][randY] = null;
+			if(attractMode){
+				setTimeout(tick,1000+(Math.floor(Math.random()*500)));
+			}
+		}
+		
+		setupAttractGame();
+		tick();
+	}
 })()
